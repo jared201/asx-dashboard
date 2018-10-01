0fb41e036d9dc73aea066c71c38863ca8c8f3cef
Jared Odulio <jared.odulio@gmail.com>
2018-10-01 12:23:32 +0800

    added index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+let sass = require('sass');
+
+express()
+  .use(express.static(path.join(__dirname, 'dist')))
+  .set('views', path.join(__dirname, 'dist'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index.ejs'))
+
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// sass.render({file: '/dist/css/styles.scss'}, function(err, result) {
+//     if (err) throw err;
+//      console.log("css", result);
+
+/* ... */
