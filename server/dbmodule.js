820746ab302540647ac4b74c28053df4b8ad43b9
Jared Odulio <jared.odulio@gmail.com>
2018-11-06 21:56:57 +0800

    added fetch

---
+++ server/dbmodule.js
@@ -0,0 +1,25 @@
+exports.dbPool = function () {
+  const {
+    Pool
+  } = require('pg')
+  console.log('connecting to db pool')
+  const pool = new Pool({
+    connectionString: process.env.HEROKU_POSTGRESQL_NAVY_URL || process.env.DATABASE_URL,
+    ssl: true
+  })
+
+  return pool
+}
+
+exports.pgQuery = function () {
+  const pgQuery = require('pg-query')
+  console.log('connecting with PGQuery...')
+  const pgQ = pgQuery.connectionParameters = process.env.HEROKU_POSTGRESQL_NAVY_URL
+  // pgQ.connectionParameters = process.env.HEROKU_POSTGRESQL_NAVY_URL || process.env.DEFAULT_URL;
+  return pgQ
+}
+
+exports.usePGPromise = function () {
+
+
+}
